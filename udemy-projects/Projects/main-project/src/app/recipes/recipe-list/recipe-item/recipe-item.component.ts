3943952f7665d48680c2f42f5c0078cc41e4011c
Matt1479 <gitmatt1479@gmail.com>
2022-06-22 17:48:27 +0200

    finish Section-06

--- udemy-projects/Projects/main-project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ udemy-projects/Projects/main-project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent implements OnInit {
+  // exposing it with @Input() to bind it, to later get data from outside
+  @Input('recipeItem') recipe: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSelected() {
+    this.recipeSelected.emit();
   }
-
 }
--- udemy-projects/Projects/main-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ udemy-projects/Projects/main-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,9 +7,16 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  // passing this array down to recipeItemComponent
   recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
+      'This is a test description',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+    ),
+    new Recipe(
+      'Another Test Recipe',
       'This is a test description',
       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
     ),
@@ -18,4 +25,8 @@ export class RecipeListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
